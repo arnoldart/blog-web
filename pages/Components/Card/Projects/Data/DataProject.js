228a4f9932358d050bdd1a8ea200c5966ff0534a
arnoldart <adoni.091203@gmail.com>
2021-02-02 20:51:16 +0700

    responsive

--- pages/Components/Card/Projects/Data/DataProject.js
+++ pages/Components/Card/Projects/Data/DataProject.js
@@ -5,9 +5,11 @@ import {tw} from 'twind'
 export default function DataProjects() {
   return(
     <>
-      <main className={tw `flex justify-evenly`}>
+      <main className={tw `flex justify-evenly items-center flex-col sm:flex-row`}>
         <CardProject {...CardOne} />
-        <CardProject {...CardOne} />
+        <div className={tw `my-10 sm:my-0`}>
+          <CardProject {...CardOne} />
+        </div>
         <CardProject {...CardOne} />
       </main>
     </>
