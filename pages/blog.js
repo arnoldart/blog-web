e198971d0e9190e2bf98d9767c0bd5c8aac4fd7f
arnoldart <adoni.091203@gmail.com>
2021-02-03 15:10:31 +0700

    blog

--- pages/blog.js
+++ pages/blog.js
@@ -1,22 +1,55 @@
 import Head from 'next/head'
 import Nav from './Components/Nav/NavBlog'
-import {tw} from 'twind'
+import {css, apply, theme, tw} from 'twind/css'
+import DataProjects from './Components/Card/Projects/Data/DataProject'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/Date'
 
-export default function Blog() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+
+export default function project({allPostsData}) {
+
   return (
     <>
       <Head>
         <title>Blog | Arnold Blog</title>
       </Head>
-      <Nav />
 
-     <main className={tw `flex justify-center`}>
+      <Nav />
+      
+      <main className={tw `container mx-auto`}>
 
         <div className='heading'>
-          <h1>My <span className={tw `text-yellow-300`}>Blog</span></h1>
+          <h1>All <span className={tw `text-yellow-300`}>Posts</span></h1>
+        </div>
+
+        <div className={tw `flex mt-20 flex-col sm:flex-row justify-evenly items-center`}>
+        {allPostsData.map(({ id, date, title }) => (
+          <div key={id} className={tw `w-80 my-10 sm:my-0 bg-gray-500 rounded-2xl mt-0 transition-all duration-500 sm:hover:-mt-10 hover:shadow-md`}>
+            <a href={`/posts/${id}`}>
+              <div>
+                <img className={tw `rounded-t-2xl`} src="" alt=""/>
+              </div>
+              <div className={tw `text-white p-4 `}>
+                <h3>{title}</h3>
+                <p>{date}</p>
+              </div>
+            </a>
+          </div>
+        ))}
         </div>
       
       </main>
-    </>
+
+    </> 
   )
 }
--- pages/project.js
+++ pages/project.js
@@ -1,22 +1,20 @@
 import Head from 'next/head'
 import Nav from './Components/Nav/NavProject'
-import {css, apply, theme, tw} from 'twind/css'
+import {tw} from 'twind'
 import DataProjects from './Components/Card/Projects/Data/DataProject'
 
-export default function project() {
-
+export default function Blog() {
   return (
     <>
       <Head>
         <title>Project | Arnold Blog</title>
       </Head>
-
       <Nav />
-      
+
       <main className={tw `container mx-auto`}>
 
         <div className='heading'>
-          <h1>My <span className={tw `text-yellow-300`}>Project</span></h1>
+          <h1>All <span className={tw `text-yellow-300`}>Project</span></h1>
         </div>
 
         <div className={tw `mt-20`}>
@@ -25,6 +23,6 @@ export default function project() {
       
       </main>
 
-    </> 
+    </>
   )
 }
