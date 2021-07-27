f2b79c133e790b8aad2dc97dc58daa1b2b11dbf4
arnoldart <adoni.091203@gmail.com>
2021-07-27 12:45:35 +0700

    update wev

--- pages/index.js
+++ pages/index.js
@@ -16,19 +16,21 @@ export default function Home() {
 
       <Nav />
 
-      <main className={tw `md:static`}>
-        <div className={tw `mx-6 xl:mx-32 bg-gray-700 rounded-lg mt-7`}>
-          <div className={tw `flex flex-col lg:flex-row items-center p-6`}>
-            <div>
-              <Image src={ProfilePc} alt="test"/>
-            </div>
-            <div className={tw `text-white text-center lg:text-left mt-10 lg:mt-0`}>
-              <p className={tw `font-bold text-3xl lg:text-3xl`}>Halo semua perkenalkan saya Arnold</p>
-              <p className={tw `mt-3`}>Seorang Junior Front End Developer, saya juga membagikan tips tips dan project sekaligus dengan source code nya.Seorang Junior Front End Developer, saya juga membagikan tips tips dan project sekaligus dengan source code nya.</p>
+      <main className={tw `flex flex-col min-h-screen`}>
+        <div className={tw `flex-1`}>
+          <div className={tw `mx-6 xl:mx-32 bg-gray-700 rounded-lg mt-7`}>
+            <div className={tw `flex flex-col lg:flex-row items-center p-6`}>
+              <div>
+                <Image src={ProfilePc} alt="test" width="512" height="512"/>
+              </div>
+              <div className={tw `text-white text-center lg:text-left mt-10 lg:ml-8 lg:mt-0`}>
+                <p className={tw `font-bold text-3xl lg:text-3xl`}>Halo semua perkenalkan saya Arnold</p>
+                <p className={tw `mt-3 text-lg text-gray-200`}>Seorang Junior Front End Developer, saya juga membagikan tips tips dan project sekaligus dengan source code nya.Seorang Junior Front End Developer, saya juga membagikan tips tips dan project sekaligus dengan source code nya.</p>
+              </div>
             </div>
           </div>
         </div>
-        <footer className={tw `md:absolute bottom-0 w-full mt-10`}>
+        <footer className={tw `mt-10`}>
           <Footer />
         </footer>
       </main>
