2c2bf1eaa64846b674c662bbc69be1424f36fc6c
Jedidta Adoni Saputra <adoni.091203@gmail.com>
2023-09-19 19:45:39 +0000

    feat: create menu button

--- Components/Navbar/Nav.tsx
+++ Components/Navbar/Nav.tsx
@@ -13,25 +13,27 @@ export default function Nav() {
         <div className={Styles.navbar_container}>
           <div className={Styles.navbar_logo}>
             <p>Arnoldart</p>
-            <button
+            <div
               onClick={() => setActive(!active)}
               className={Styles.button_mobile_menu}
             >
-              ini tombol
-            </button>
+              <span className={`${Styles.line_1} ${active && Styles.line_1_active}`}></span>
+              <span className={`${Styles.line_2} ${active && Styles.line_2_active}`}></span>
+              <span className={`${Styles.line_3} ${active && Styles.line_3_active}`}></span>
+            </div>
           </div>
           <div className={`${Styles.navbar_menu} ${ active ? Styles.navbar_menu_active : Styles.navbar_menu_inactive}`}>
             <Link href={"/"} className={`${Styles.navbar_link}`}>
-              <p className={router.pathname == "/" ? Styles.navbar_link_active : ""}>Home</p>
+              <p className={`${router.pathname == "/" && Styles.navbar_link_active}`}>Home</p>
             </Link>
             <Link href={"/About"} className={Styles.navbar_link}>
-              <p className={router.pathname == "/About" ? Styles.navbar_link_active : ""}>About Me</p>
+              <p className={`${router.pathname == "/About" && Styles.navbar_link_active}`}>About Me</p>
             </Link>
             <Link href={"/Project"} className={Styles.navbar_link}>
-              <p className={router.pathname == "/Project" ? Styles.navbar_link_active : ""}>Project</p>
+              <p className={`${router.pathname == "/Project" && Styles.navbar_link_active}`}>Project</p>
             </Link>
             <Link href={"/Blog"} className={Styles.navbar_link}>
-              <p className={router.pathname == "/Blog" ? Styles.navbar_link_active : ""}>Blog</p>
+              <p className={`${router.pathname == "/Blog" && Styles.navbar_link_active}`}>Blog</p>
             </Link>
           </div>
         </div>
