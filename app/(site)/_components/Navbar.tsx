64e6c20a60b1f8e856d6e95b7c5a9a7f1da54282
Jedidta Adoni Saputra <adoni.091203@gmail.com>
2023-12-30 02:51:14 +0000

    fix: resolve navbar stylikng

--- app/(site)/_components/Navbar.tsx
+++ app/(site)/_components/Navbar.tsx
@@ -4,25 +4,42 @@ import { ModeToggle } from "~/components/mode-toggle"
 import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons"
 import { Button } from "~/components/ui/button"
 import Link from "next/link"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useScrollTop } from "~/hooks/use-scroll-top"
 import { cn } from "~/lib/utils"
 
 const Navbar = () => {
-  const [click, setClick] = useState(true)
+  const [click, setClick] = useState(false)
   const handleClick = () => setClick(!click)
+  const menuRef = useRef(null);
+
+  const handleMenuItemClick = () => setClick(false);
+
+  const handleOutsideClick = (e: MouseEvent) => {
+    if (menuRef.current && !(menuRef.current as any).contains(e.target)) {
+      setClick(false);
+    }
+  };
+
+  useEffect(() => {
+    // Attach an event listener to handle outside clicks
+    document.addEventListener("click", handleOutsideClick);
+
+    // Clean up the event listener when the component is unmounted
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
   // const scrolled = useScrollTop()
 
   return (
-    <div>
-      <div className="block md:flex items-center justify-between py-5">
-        <div className="flex items-center justify-between md:w-auto w-full static z-50">
-          <Link href='/'>
-            <p>Arnoldart</p>
-          </Link>
-          <p onClick={handleClick} className="md:hidden block">menu</p>
-        </div>
-        <ul className={`${click ? 'flex-row' : 'flex-col h-screen justify-center items-center absolute m-auto w-full bg-gray-700 top-0 z-40'} flex items-center gap-x-3`}>
+    <div id="navbar" className="relative md:flex justify-between block md:py-5">
+      <div className="flex justify-between md:px-0 px-5 md:py-0 py-5">
+        <p style={{ zIndex: "5" }}>Arnoldart</p>
+        <p onClick={handleClick} style={{ zIndex: "5" }} className="md:hidden block">menu</p>
+      </div>
+      <div className="md:block hidden">
+        <ul className="flex items-center gap-x-5">
           <li>
             <Link href='/'>
               Home
@@ -44,22 +61,34 @@ const Navbar = () => {
             </Link>
           </li>
         </ul>
-        <div className="md:flex hidden items-center">
-          <Button size="icon" variant="ghost">
-            <Link target="_blank" href="https://www.linkedin.com/in/jedidta-adoni-saputra/">
-              <LinkedInLogoIcon className="h-4 w-4" />
-            </Link>
-          </Button>
-          <Button size="icon" variant="ghost">
-            <Link target="_blank" href="https://github.com/arnoldart/">
-              <GitHubLogoIcon className="h-4 w-4" />
-            </Link>
-          </Button>
-          <ModeToggle />
-        </div>
-        {/* <div className={cn("absolute w-full h-screen bg-slate-500 top-0 opacity-0 transition-all ease-out duration-300", click && "opacity-1")}>
-          adasd
-        </div> */}
+      </div>
+      <div className="md:hidden block">
+        {click && (
+          <div className="flex flex-col justify-center items-center bg-gray-400 absolute h-screen w-full top-0">
+            <ul className="text-center">
+              <li>
+                <Link href='/'>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link href='/Project'>
+                  Project
+                </Link>
+              </li>
+              <li>
+                <Link href="/Blog">
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link href="/AboutMe">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   )
